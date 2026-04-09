858abf084533d7cf0e72799caf671d619951e263
Mark Platt <mark@mrkplt.com>
2026-04-08 21:31:26 -0400

    Set Devise mailer sender to Mailgun sandbox address

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
+  config.mailer_sender = "noreply@sandboxec5b77d0bde14b05b0602d251740d443.mailgun.org"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
