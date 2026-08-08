a308a56055ce4be4bffeccf041b3b023b5b1a2fb
Mark Platt <mark@mrkplt.com>
2026-08-08 11:29:59 -0400

    Revert icon helper - Icons gem has viewBox case bug
    The Icons gem's transformation logic lowercases viewBox to viewbox,
    which breaks SVG. The gem uses Nokogiri::HTML which lowercases
    attributes. We must keep runtime gsub calls until the gem fixes this.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,13 +7,16 @@ module ApplicationHelper
     icon = Icons::Icon.new(name: name, library: library, arguments: html_options)
     svg = icon.svg
 
+    # Replace hardcoded colors with currentColor so CSS can control the color
+    # NOTE: Icons gem transformations lowercase viewBox, so we must do this at runtime
+    svg = svg.gsub(/stroke="#[0-9A-Fa-f]{6}"/, 'stroke="currentColor"')
+    svg = svg.gsub(/fill="#[0-9A-Fa-f]{6}"/, 'fill="currentColor"')
+
     # Add class attribute if provided
     if html_options[:class].present?
       svg = svg.sub(/<svg/, "<svg class=\"#{html_options[:class]}\"")
     end
 
-    # Icons gem now returns SVGs with currentColor (configured in initializer)
-    # Sanitize to satisfy Rails/OutputSafety cop
     svg.html_safe # rubocop:disable Rails/OutputSafety
   end
 
